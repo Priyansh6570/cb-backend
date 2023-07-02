66375ec20982a8fd51d83e7d8e6da4bc8cd11761
Priyansh Mishra <priyanshmishra20052003@gmail.com>
2023-07-02 13:46:00 +0530

    load user fix

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,12 +5,12 @@ const sendToken = (user, statusCode, res) => {
     const token = user.getJwtToken();
     // Options for cookie
     const options = {
-        expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
-        ),
+        expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        sameSite: 'none', // must be 'none' to enable cross-site delivery
-    };
+        sameSite: 'None',
+        secure: true,
+      };
+      
     res.status(statusCode).cookie('token', token, options).json({
         success: true,
         token,
