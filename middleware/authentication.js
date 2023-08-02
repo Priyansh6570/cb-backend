ac13f385643c2a4acad06f3823bce59c100205f3
Priyansh Mishra <priyanshmishra20052003@gmail.com>
2023-08-02 09:06:26 +0530

    auth change

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -29,8 +29,8 @@ const isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
   // Check if the token exists in the request headers
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
     token = req.headers.authorization.split(' ')[1];
-  } else if (req.cookies) {
-    token = req.cookies;
+  } else if (req.cookies.token) {
+    token = req.cookies.token;
   }
 
     if (!token) {
